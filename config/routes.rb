09f865ea804846b7b5b308607142320db76c9757
Bishizel <bishizel@gmail.com>
2010-02-19 23:02:44 -0600

    Added a pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Sharktank::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
