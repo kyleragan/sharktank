cb9cae386e4949594baaabbc62596daae4549229
Kyle Ragan <kyle.ragan@gmail.com>
2011-02-19 21:16:18 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharktank::Application.config.secret_token = '5162d2554dc7e92338bc7e60e089ca0969b8eb08e44063acc708f5d84017b8792f99e3b824a37555c1c5b9c95922cdd1a961a4728c45902542fe62fa2348eed1'
